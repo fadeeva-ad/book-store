5652c3bb7e385e7678a1dee8b88f112183caa8a3
Fadeeva <fadeevaad1@gmail.com>
2018-07-22 13:16:19 +0300

    was added layout-page on main page

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+svg4everybody(); // иницализация полифила для IE
+
+$(document).ready(function(){
+  // весь ваш код c jQuery
+});
+
+// Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
+// function ready(fn) {
+//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+//
+// ready(function(){
+//   // code
+// });
